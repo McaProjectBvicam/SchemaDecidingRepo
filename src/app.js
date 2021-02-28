95d2c323574fdc3b9a51e418bdeaa8d829c38743
lakshay <lakshay250407@gmail.com>
2021-02-28 16:45:06 +0530

    final commit

--- src/app.js
+++ src/app.js
@@ -16,8 +16,7 @@ const partials_path= path.join(__dirname, "../templates/partials");
 //to fetch our form values; without below 2 statements data entered by users is not gonna display on our page!
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+
 
 app.use(express.static(static_path));
 app.set("view engine","hbs");
