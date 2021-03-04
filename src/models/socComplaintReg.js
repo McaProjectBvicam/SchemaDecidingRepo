5432e2522a9be96e27ecd3a10f91e32c36866609
sharmaji238 <ravisharma2382000@gmail.com>
2021-03-05 01:27:40 +0530

    nishtha final

---
+++ src/models/socComplaintReg.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const socComplaintSchema = new mongoose.Schema({
+    societyName: {
+        type: String,
+        require: true
+    },
+    societyMemberName: {
+        type: String,
+        require: true
+    },
+
+    
+        type: String,
+        require: true
+    },
+
+    complaintSubject: {
+        type: String,
+        require: true
+    },
+    complaintDesc: {
+        type: String,
+        require: true
+    },
+    complaintDate: {
+        type: Date,
+        require: true
+    },
+    complaintStatus: {
+        type: String,
+        require: true
+    },
+
+
+
+})
+
+const complaintRegister = new mongoose.model("SocietyComplaint",socComplaintSchema);
+module.exports = complaintRegister;
